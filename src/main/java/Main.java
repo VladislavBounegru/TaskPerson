a7e74c07697fe57752e4d6b30b88b3e9bb492b4c
18743416 <vvbounegru@sberbank.ru>
2020-10-26 15:02:11 -0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Incorrect number of arguments");
+        } else {
+            Person personFromArgs = getPersonFromArgs(args);
+            System.out.println(personFromArgs);
+        }
+    }
+
+    public static Person getPersonFromArgs(String[] args) {
+        return new Person(args[0], args[1]);
+    }
+
+}
