f321c91f30057462c4336c6a9349707f2a886549
18743416 <vvbounegru@sberbank.ru>
2020-10-26 16:19:03 -0700

    complete task#4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Person person = getPerson();
-        System.out.println(person);
-
+        List<Person> people = new ArrayList<Person>();
+        boolean exit = false;
+        Scanner scanner = new Scanner(System.in);
+        while (!exit) {
+            showMenu();
+            String input = scanner.nextLine();
+            switch (input) {
+                case "1":
+                    people.add(getPerson());
+                    break;
+                case "2":
+                    System.out.println(people);
+                    break;
+                case "3":
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Wrong input. Try again");
+                    break;
+            }
+        }
+        scanner.close();
     }
 
+
+    public static void showMenu() {
+        System.out.println("Menu:");
+        System.out.println("1.Add");
+        System.out.println("2.Show");
+        System.out.println("3.Exit");
+    }
 
     public static Person getPerson() {
         Scanner scanner = new Scanner(System.in);
