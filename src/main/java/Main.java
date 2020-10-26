c200184529634e5924e0fd07fe589efd5f920743
18743416 <vvbounegru@sberbank.ru>
2020-10-26 15:13:55 -0700

    complete task#2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,21 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Incorrect number of arguments");
-        } else {
-            Person personFromArgs = getPersonFromArgs(args);
-            System.out.println(personFromArgs);
-        }
+        Person person = getPerson();
+        System.out.println(person);
+
     }
 
-    public static Person getPersonFromArgs(String[] args) {
-        return new Person(args[0], args[1]);
+
+    public static Person getPerson() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first name: ");
+        String firstName = scanner.next();
+        System.out.print("Enter last name: ");
+        String secondName = scanner.next();
+        return new Person(firstName, secondName);
     }
 
 }
